165832245b345f27891b78207c2c395235ece2ce
Movses5555 <mosisrayelyan@gmial.com>
2025-02-06 15:40:20 +0400

    aaaaaa55555555

--- src/components/TelBut.jsx
+++ src/components/TelBut.jsx
@@ -1,19 +1,82 @@
-import { useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 
-const TelegramLogin = () => {
+const TelegramLoginButton = ({
+  wrapperProps,
+  dataAuthUrl,
+  usePic = false,
+  botName,
+  className,
+  buttonSize = "large",
+  dataOnauth,
+  cornerRadius,
+  requestAccess = true,
+}) => {
+  const ref = useRef(null);
+
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://telegram.org/js/telegram-widget.js?7";
-    script.async = true;
-    script.setAttribute("data-telegram-login", "okvpn_xbot");
-    script.setAttribute("data-size", "large");
-    script.setAttribute("data-userpic", "true");
-    script.setAttribute("data-onauth", "onTelegramAuth(user)");
-    script.setAttribute("data-request-access", "write");
-    document.getElementById("telegram-login-container")?.appendChild(script);
-  }, []);
+    if (typeof window === "undefined" || ref.current === null) return;
 
-  return <div id="telegram-login-container"></div>;
+    if (
+      typeof dataOnauth === "undefined" &&
+      typeof dataAuthUrl === "undefined"
+    ) {
+      throw new Error(
+        "One of these props should be defined: dataAuthUrl (redirect URL), dataOnauth (callback fn)."
+      );
+    }
+
+    if (typeof dataOnauth === "function") {
+      window.TelegramLoginWidget = {
+        dataOnauth: (user) => dataOnauth(user),
+      };
+    }
+
+    const script = document.createElement("script");
+    script.src = "https://telegram.org/js/telegram-widget.js?22";
+    script.setAttribute("data-telegram-login", botName);
+    script.setAttribute("data-size", buttonSize);
+
+    if (cornerRadius !== undefined) {
+      script.setAttribute("data-radius", cornerRadius.toString());
+    }
+
+    if (requestAccess) {
+      script.setAttribute("data-request-access", "write");
+    }
+
+    script.setAttribute("data-userpic", usePic.toString());
+
+    if (typeof dataAuthUrl === "string") {
+      script.setAttribute("data-auth-url", dataAuthUrl);
+    } else {
+      script.setAttribute(
+        "data-onauth",
+        "TelegramLoginWidget.dataOnauth(user)"
+      );
+    }
+
+    script.async = true;
+    ref.current.appendChild(script);
+  }, [
+    botName,
+    buttonSize,
+    cornerRadius,
+    dataOnauth,
+    requestAccess,
+    className,
+    usePic,
+    ref,
+    dataAuthUrl,
+  ]);
+
+  return (
+    <div
+      ref={ref}
+      className={className}
+      {...wrapperProps}
+    >
+    </div>
+  )
 };
 
-export default TelegramLogin;
+export default TelegramLoginButton;
